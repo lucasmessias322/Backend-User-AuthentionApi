39ab999989038bf4365134ed5ae88a94b7847207
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2021-12-17 14:01:53 -0300

    ..........

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
   app.use(cors());
   next();
 });
